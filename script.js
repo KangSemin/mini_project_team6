1eaf241ec43ba58ee32e9f841c573e1c914584ab
강세민 <semin15151@gmail.com>
2024-11-07 11:45:20 +0900

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const characters = document.querySelectorAll('.character');
     const originalTextElements = document.querySelectorAll('.original-text');
-
     const hideElements = (elements, delay) => {
         elements.forEach(element => {
             element.style.opacity = '0';
@@ -10,33 +9,30 @@ document.addEventListener('DOMContentLoaded', function () {
             }, delay);
         });
     };
-
     const showElements = (elements) => {
         elements.forEach(element => {
             element.style.display = 'flex';  // flex로 변경하여 이미지와 텍스트가 같이 보이게
             element.style.opacity = '0';
-            setTimeout(() => { element.style.opacity = '1'; }, 10);
+            setTimeout(() => {
+                element.style.opacity = '1';
+            }, 0); // 속도를 더 빠르게 하기 위해 짧은 시간으로 변경
         });
     };
-
     characters.forEach((character, index) => {
         // `new-text1`부터 `new-text4`까지 처리
         const newTextElements = document.querySelectorAll(`.new-text${index + 1}`);
-
         character.addEventListener('mouseenter', function () {
-            hideElements(originalTextElements, 500);
+            hideElements(originalTextElements, 200); // 속도를 빠르게 하기 위해 시간 단축
             setTimeout(() => {
                 showElements(newTextElements);
-            }, 500);
+            }, 200); // 속도를 빠르게 하기 위해 시간 단축
         });
-
         character.addEventListener('mouseleave', function () {
-            hideElements(newTextElements, 500);
+            hideElements(newTextElements, 200); // 속도를 빠르게 하기 위해 시간 단축
             setTimeout(() => {
                 showElements(originalTextElements);
-            }, 500);
+            }, 200); // 속도를 빠르게 하기 위해 시간 단축
         });
-
         character.addEventListener('click', function () {
             const link = character.getAttribute('data-link');
             window.location.href = link;
